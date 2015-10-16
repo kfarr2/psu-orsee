89ad29e11dcb800b50e8c87b145507cac33656d9
Konstantin <kfarr2@pdx.edu>
2015-10-16 11:55:47 -0700

    Document CAS authentication method

--- utils/general.php
+++ utils/general.php
@@ -52,14 +52,15 @@ function cas_authenticate($url, $conn, $ticket=NULL){
 
     // this user doesn't exist. Add to the database.
     if(!$row){
+        // Assign some variables.
         $now = date('Ymd');
-        $pending_update = true;
+        $pending_update = 'y';
         $language = "en";
         $status_id = 0;
         $subscriptions = "|1|,|2|"; // students get subscribed to everything
         $new_id = participant__create_participant_id($tokens);
-        $SQL = 'INSERT INTO or_participants
-                    (
+        // Prepare the SQL statement
+        $SQL = 'INSERT INTO or_participants (
                         subpool_id,
                         subscriptions,
                         rules_signed,
@@ -73,13 +74,15 @@ function cas_authenticate($url, $conn, $ticket=NULL){
                         confirmation_token,
                         participant_id,
                         participant_id_crypt
-                    ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
+                ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
+
+        // Bind the params
         $result = $conn->prepare($SQL);
         $result->bindParam(1, $_SESSION['subpool_id'], PDO::PARAM_INT);
         $result->bindParam(2, $subscriptions, PDO::PARAM_STR);
         $result->bindParam(3, $_SESSION['rules'], PDO::PARAM_STR);
         $result->bindParam(4, $status_id, PDO::PARAM_INT);
-        $result->bindParam(5, $pending_update, PDO::PARAM_INT);
+        $result->bindParam(5, $pending_update, PDO::PARAM_STR);
         $result->bindParam(6, $language, PDO::PARAM_STR);
         $result->bindParam(7, $email, PDO::PARAM_STR);
         $result->bindParam(8, $tokens[0], PDO::PARAM_STR);
@@ -89,6 +92,11 @@ function cas_authenticate($url, $conn, $ticket=NULL){
         $result->bindParam(12, $new_id['participant_id'], PDO::PARAM_STR);
         $result->bindParam(13, $new_id['participant_id_crypt'], PDO::PARAM_STR);
         $outcome = $result->execute();
+        // DB operation failed somehow
+        if(!$outcome){
+            exit("An error occurred. Please try again later or contact the system administrator if the issue persists.");
+        }
+        // Get the freshly added row from the database and send the confirmation email.
         $query->execute();
         $row = $query->fetch();
         experimentmail__confirmation_mail($row);
@@ -97,7 +105,7 @@ function cas_authenticate($url, $conn, $ticket=NULL){
     // Error if inactive user
     if($row['locked'] == 1) exit("User is inactive. Please contact the system administrator.");
 
-    // User exists in out DB, so just use that one.
+    // User exists in our DB, so just use that one.
     setcookie("cookieUserName", $user, time()+60*60*24*365);
     return $row;
 }
